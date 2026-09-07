2d9cec6c47be8937475b6a0dc9ac171691759bf0
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-09-07 23:09:52 +0530

    ✅ The PADS | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ hackerrank/medium/the-pads/solution.sql
@@ -0,0 +1,9 @@
+/*
+Enter your query here.
+*/
+
+select concat(name,'(',left(occupation,1),')')  from occupations order by name;
+
+select concat('There are a total of',' ',count(*),' ',lower(occupation),'s.') from occupations
+group by occupation order by count(*),occupation asc;
+ 
