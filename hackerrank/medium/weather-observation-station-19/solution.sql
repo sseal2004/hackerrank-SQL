16de43edc5d4bb10530941f8d986a4609d9c21ad
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-09-08 09:52:35 +0530

    ✅ Weather Observation Station 19 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-09-08

--- hackerrank/medium/weather-observation-station-19/solution.sql
+++ hackerrank/medium/weather-observation-station-19/solution.sql
@@ -1,5 +1,4 @@
 /*
 Enter your query here.
 */
-
-select round(abs(max(lat_n)-min(lat_n))+abs(max(long_w)-min(long_w)),4) from station 
+select round(sqrt(power(max(lat_n)-min(lat_n),2)+power(max(long_w)-min(long_w),2)),4) from station
