d3eae9bbbeeadede656bf18426c1e1de221bc22b
Soumyadipta Seal <146237831+sseal2004@users.noreply.github.com>
2026-09-08 09:29:16 +0530

    ✅ Weather Observation Station 17 | Easy | SQL | Runtime: N/A | Memory: N/A | 2026-09-08

--- hackerrank/easy/weather-observation-station-17/solution.sql
+++ hackerrank/easy/weather-observation-station-17/solution.sql
@@ -1,4 +1,4 @@
 /*
 Enter your query here.
 */
-select round(min(lat_n),4) from station where lat_n > 38.7780 
+select round((long_w),4) from station where lat_n > 38.7780 order by lat_n asc limit 1
